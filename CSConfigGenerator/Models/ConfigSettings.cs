9f016c56a8fd0bb4e2bf94760216fb8b15035005
Kjell Reidar Joessang <95139113+Tinnitussen@users.noreply.github.com>
2025-07-26 14:21:21 +0200

    feat: add two way syncing between settings and editor

---
+++ CSConfigGenerator/Models/ConfigSettings.cs
@@ -0,0 +1,7 @@
+namespace CSConfigGenerator.Models;
+public class ConfigSettings
+{
+    public float MouseSensitivity { get; set; } = 1.0f;
+    public string CrossHairColor { get; set; } = "green";
+    public bool EnableVSync { get; set; } = true;
+}
--- CSConfigGenerator/Program.cs
+++ CSConfigGenerator/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using CSConfigGenerator;
+using CSConfigGenerator.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton<ConfigStateService>();
 
 await builder.Build().RunAsync();
