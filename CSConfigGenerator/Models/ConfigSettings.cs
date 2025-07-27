b3e0b452c970e8c1dc55aadd7c1173a9a18c4904
Kjell Reidar Joessang <95139113+Tinnitussen@users.noreply.github.com>
2025-07-27 08:00:36 +0200

    refactor: remove unused configsettings.cs

--- CSConfigGenerator/Models/ConfigSettings.cs
+++
@@ -1,7 +0,0 @@
-namespace CSConfigGenerator.Models;
-public class ConfigSettings
-{
-    public float MouseSensitivity { get; set; } = 1.0f;
-    public string CrossHairColor { get; set; } = "green";
-    public bool EnableVSync { get; set; } = true;
-}
